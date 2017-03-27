b03b1870eb1814996964fc01622dd1120a0d0608
Shamil Chollampatt <shamil.cm@gmail.com>
2017-03-27 09:29:37 +0800

    added debpe for LM feature

--- features.py
+++ features.py
@@ -17,19 +17,22 @@ logger = logging.getLogger(__name__)
 
 ln10 = math.log(10)
 class LM:
-    def __init__(self, feature_name, path, normalize=False):
+    def __init__(self, feature_name, path, normalize=False, debpe=False):
         self.path = path
         c = kenlm.Config()
         c.load_method = kenlm.LoadMethod.LAZY
         self.model = kenlm.Model(path, c)
         self.name = feature_name
         self.normalize = normalize
+        self.debpe = debpe
         logger.info('Intialized ' + str(self.model.order) + "-gram language model: " + path)
 
     def get_name(self):
         return self.name
 
     def get_score(self, source, candidate):
+        if self.debpe:
+            candidate = candidate.replace('@@ ','')
         lm_score = self.model.score(candidate)
         log_scaled = round(lm_score*ln10,4)
         if self.normalize == True:
